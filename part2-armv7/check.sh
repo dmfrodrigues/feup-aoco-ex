670f78d5764af27157d7e581e4275c757f500683
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2020-10-12 18:49:31 +0100

    Fixed minor issues

--- part2-armv7/check.sh
+++ part2-armv7/check.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-java -jar /usr/local/bin/visual/content/visual_headless.jar --headless $1 --td:abort --syntax --runtime --mode:completion > $1.out
-if grep -q "<syntax-error" $1_log.xml ; then
+java -jar /usr/local/bin/visual/content/visual_headless.jar --headless "$1" --td:abort --syntax --runtime --mode:completion > "$1".out
+if grep -q "<syntax-error" "$1_log.xml" ; then
     exit 1
 fi
-if grep -q "<runtime-error" $1_log.xml ; then
+if grep -q "<runtime-error" "$1_log.xml" ; then
     exit 1
 fi
-if grep -q "WARNING: Possible infinite loop detected!" $1.out ; then
+if grep -q "WARNING: Possible infinite loop detected!" "$1.out" ; then
     exit 1
 fi
